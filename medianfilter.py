1c447c3b175d17a14bc4ff498bac2fd9565ca4a9
yliwang <1220264221@qq.com>
2021-11-04 19:17:49 +0800

    Update medianfilter.py

--- medianfilter.py
+++ medianfilter.py
@@ -1,6 +1,9 @@
 # python image median filtering
 
-def MedianFilter( f, KernelSize ):
+def MedianFilter( f, KernelSize ): # f is the image to be filtered, 
+                                   # KernelSize is the size of the filter, 
+                                   # KernelSize is an odd number, 
+                                   # for example, if KernelSize=3, then the median filter is 3x3
 
 
 def main( fn1, fn2):  
